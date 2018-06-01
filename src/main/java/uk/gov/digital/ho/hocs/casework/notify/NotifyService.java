9656632462517faf16b35594b1e40475c85f568a
lucascairns <lucas.cairns@digital.homeoffice.gov.uk>
2018-06-01 13:46:34 +0100

    Added tests

--- src/main/java/uk/gov/digital/ho/hocs/casework/notify/NotifyService.java
+++ src/main/java/uk/gov/digital/ho/hocs/casework/notify/NotifyService.java
@@ -5,11 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import uk.gov.digital.ho.hocs.casework.model.NotifyRequest;
-import uk.gov.service.notify.NotificationClient;
 import uk.gov.service.notify.NotificationClientException;
 
-import java.net.InetSocketAddress;
-import java.net.Proxy;
 import java.util.HashMap;
 import java.util.UUID;
 
@@ -18,29 +15,18 @@ import java.util.UUID;
 @Slf4j
 public class NotifyService {
 
+    private final NotifyClient notify;
+
     private final String rshTemplateId;
     private final String frontEndUrl;
 
-    private final NotificationClient client;
-
     @Autowired
-    public NotifyService(@Value("${notify.apiKey}") String apiKey,
+    public NotifyService(NotifyClient notifyClient,
                          @Value("${notify.rshTemplateId}") String rshTemplateId,
-                         @Value("${notify.frontend.url}") String frontEndUrl,
-                         @Value("${notify.proxy.host}") String proxyHost,
-                         @Value("${notify.proxy.port}") Integer proxyPort) {
+                         @Value("${notify.frontend.url}") String frontEndUrl) {
+        this.notify = notifyClient;
         this.rshTemplateId = rshTemplateId;
         this.frontEndUrl = frontEndUrl;
-
-        if (proxyHost != null && proxyPort != null) {
-            Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(proxyHost, proxyPort));
-            client = new NotificationClient(apiKey, proxy);
-            log.info("Using proxy notify");
-        } else {
-            client = new NotificationClient(apiKey);
-            log.info("Not using proxy notify");
-        }
-
     }
 
     public void sendRshNotify(NotifyRequest notifyRequest, UUID caseUUID) {
@@ -50,17 +36,17 @@ public class NotifyService {
     public void sendNotify(NotifyRequest notifyRequest, UUID caseUUID, String templateId) {
         log.info("Received request to email {} {}, templateId {}", notifyRequest.getNotifyEmail(), notifyRequest.getNotifyEmail(), templateId);
         if (notifyRequest.getNotifyEmail() != null && !notifyRequest.getNotifyEmail().isEmpty()) {
-            sendEmail(notifyRequest.getNotifyEmail(), notifyRequest.getNotifyTeamName(),caseUUID, templateId);
+            sendEmail(notifyRequest.getNotifyEmail(), notifyRequest.getNotifyTeamName(), caseUUID, templateId);
         }
     }
 
-    private void sendEmail(String emailAddress, String teamName,UUID caseUUID, String templateId) {
+    private void sendEmail(String emailAddress, String teamName, UUID caseUUID, String templateId) {
         log.info("Sending email to {} {}, templateId {}", emailAddress, teamName, templateId);
         HashMap<String, String> personalisation = new HashMap<>();
         personalisation.put("team", teamName);
         personalisation.put("link", frontEndUrl + "/case/" + caseUUID);
         try {
-            client.sendEmail(templateId, emailAddress, personalisation, null, null);
+            notify.getClient().sendEmail(templateId, emailAddress, personalisation, null, null);
         } catch (NotificationClientException e) {
             e.printStackTrace();
         }
---
+++ src/test/java/uk/gov/digital/ho/hocs/casework/caseDetails/CaseResourceTest.java
@@ -0,0 +1,70 @@
+package uk.gov.digital.ho.hocs.casework.caseDetails;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import uk.gov.digital.ho.hocs.casework.model.CaseCreateRequest;
+import uk.gov.digital.ho.hocs.casework.model.CaseSaveResponse;
+import uk.gov.digital.ho.hocs.casework.model.UpdateStageRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CaseResourceTest {
+
+    @Mock
+    private CaseService caseService;
+
+    private CaseResource caseResource;
+
+    private final UUID uuid = UUID.randomUUID();
+    private final Map<String, Object> data = new HashMap<>();
+    private final String testUser = "Test User";
+
+    @Before
+    public void setUp() {
+        caseResource = new CaseResource(caseService);
+    }
+
+    @Test
+    public void shouldCreateCase() {
+        final String caseType = "Case Type";
+
+        when(caseService.createCase(any(), any())).thenReturn(new CaseDetails(caseType, 1234L));
+        CaseCreateRequest request = new CaseCreateRequest(caseType);
+        ResponseEntity response = caseResource.createCase(request, testUser);
+
+        verify(caseService).createCase(caseType, testUser);
+        assertThat(response).isNotNull();
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody()).isNotNull();
+        assertThat(response.getBody()).isInstanceOf(CaseSaveResponse.class);
+    }
+
+    @Test
+    public void shouldUpdateCase() {
+        final String stageName = "Stage Name";
+        final String stageData = "{stage: data}";
+
+        when(caseService.updateStage(any(UUID.class), anyInt(), anyMap(), anyString())).thenReturn(
+                new StageDetails(
+                        UUID.randomUUID(), stageName, 1, stageData)
+        );
+        UpdateStageRequest request = new UpdateStageRequest(uuid, 1, data);
+        ResponseEntity response = caseResource.updateCase(UUID.randomUUID(), request, testUser);
+
+        verify(caseService).updateStage(uuid, 1, data, testUser);
+        assertThat(response).isNotNull();
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
+
+}
