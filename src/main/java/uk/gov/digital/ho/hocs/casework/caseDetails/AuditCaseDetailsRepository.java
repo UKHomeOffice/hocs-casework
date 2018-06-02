4f5d7daefa6dcdd8bf1dab6d5ba1bd2b1535fd1e
Nick Mcmahon <nick.mcmahon@digital.homeoffice.gov.uk>
2018-06-02 01:30:16 +0100

    remove schema names from custom SQL

--- src/main/java/uk/gov/digital/ho/hocs/casework/caseDetails/AuditCaseDetailsRepository.java
+++ src/main/java/uk/gov/digital/ho/hocs/casework/caseDetails/AuditCaseDetailsRepository.java
@@ -11,6 +11,6 @@ import java.util.UUID;
 @Repository
 public interface AuditCaseDetailsRepository extends CrudRepository<AuditCaseData, String> {
 
-    @Query(value = "select cd.* from casework.audit_case_data cd join (select max(c.id) as id, c.uuid as uuid from casework.audit_case_data c where c.type in ?3 and c.created between ?1 and ?2 group by c.uuid ) scd on cd.id = scd.id order by cd.created desc", nativeQuery = true)
+    @Query(value = "select cd.* from audit_case_data cd join (select max(c.id) as id, c.uuid as uuid from audit_case_data c where c.type in ?3 and c.created between ?1 and ?2 group by c.uuid ) scd on cd.id = scd.id order by cd.created desc", nativeQuery = true)
     Set<AuditCaseData> getAllByTimestampBetweenAndCorrespondenceTypeIn(LocalDateTime start, LocalDateTime end, String[] correspondenceTypes);
 }
