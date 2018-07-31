5d7fcbd7c0b6e17456a19948bd16dbb98ecbce76
Nick McMahon <nick.mcmahon@digital.homeoffice.gov.uk>
2018-07-31 11:14:22 +0100

    Update V1_9__CREATE_TABLE_SCREEN.sql

--- src/main/resources/db/migration/postgresql/V1_9__CREATE_TABLE_SCREEN.sql
+++ src/main/resources/db/migration/postgresql/V1_9__CREATE_TABLE_SCREEN.sql
@@ -1,4 +1,5 @@
 DROP TABLE IF EXISTS screen_data;
+
 CREATE TABLE IF NOT EXISTS screen_data
 (
   id         BIGSERIAL PRIMARY KEY,
@@ -7,5 +8,6 @@ CREATE TABLE IF NOT EXISTS screen_data
   stage_uuid UUID  NOT NULL,
   CONSTRAINT fk_screen_id FOREIGN KEY (stage_uuid) REFERENCES stage_data (uuid)
 );
+
 CREATE INDEX idx_screen_type_stage_uuid
   ON screen_data (type, stage_uuid);
