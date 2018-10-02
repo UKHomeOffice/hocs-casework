6033b35f35d712f1448b7a1a23447cf05b4c9bc6
simonmitchell <simon.mitchell@digital.homeoffice.gov.uk>
2018-10-02 12:16:18 +0100

    added get primary topic from case

--- src/main/java/uk/gov/digital/ho/hocs/casework/casedetails/InputDataResource.java
+++ src/main/java/uk/gov/digital/ho/hocs/casework/casedetails/InputDataResource.java
@@ -6,10 +6,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import uk.gov.digital.ho.hocs.casework.casedetails.dto.GetPrimaryTopicResponse;
 import uk.gov.digital.ho.hocs.casework.casedetails.dto.GetInputDataResponse;
 import uk.gov.digital.ho.hocs.casework.casedetails.model.InputData;
 
 import java.util.UUID;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE;
 
 @Slf4j
 @RestController
@@ -26,5 +29,11 @@ class InputDataResource {
     public ResponseEntity<GetInputDataResponse> getInputData(@PathVariable UUID caseUUID) {
         InputData inputData = inputDataService.getInputData(caseUUID);
         return ResponseEntity.ok(GetInputDataResponse.from(inputData));
+    }
+
+    @GetMapping(value = "/case/{caseUUID}/topiclist", produces = APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<GetPrimaryTopicResponse> getPrimaryTopicForCase(@PathVariable UUID caseUUID) {
+        InputData inputData = inputDataService.getInputData(caseUUID);
+        return ResponseEntity.ok(GetPrimaryTopicResponse.from(inputData));
     }
 }
