cb8f88bb859bfbc7d611318c414bc0617dfa1ba8
Stuart Whitlam <stuart.whitlam@digital.homeoffice.gov.uk>
2018-12-10 09:43:55 +0000

    Merge conflict

--- src/main/java/uk/gov/digital/ho/hocs/casework/api/StageService.java
+++ src/main/java/uk/gov/digital/ho/hocs/casework/api/StageService.java
@@ -3,6 +3,7 @@ package uk.gov.digital.ho.hocs.casework.api;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import uk.gov.digital.ho.hocs.casework.domain.exception.ApplicationExceptions;
 import uk.gov.digital.ho.hocs.casework.domain.model.Stage;
 import uk.gov.digital.ho.hocs.casework.domain.repository.StageRepository;
