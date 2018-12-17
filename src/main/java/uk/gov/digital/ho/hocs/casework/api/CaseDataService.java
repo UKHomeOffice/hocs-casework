d81ba6771729334c90d42376587a21dd23ed619b
Stuart Whitlam <stuart.whitlam@digital.homeoffice.gov.uk>
2018-12-17 08:17:29 +0000

    resolve merge conflicts

--- src/main/java/uk/gov/digital/ho/hocs/casework/api/CaseDataService.java
+++ src/main/java/uk/gov/digital/ho/hocs/casework/api/CaseDataService.java
@@ -4,8 +4,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import uk.gov.digital.ho.hocs.casework.api.dto.CaseSummary;
-import uk.gov.digital.ho.hocs.casework.api.dto.CorrespondentDto;
 import uk.gov.digital.ho.hocs.casework.client.auditclient.AuditClient;
 import uk.gov.digital.ho.hocs.casework.client.infoclient.InfoClient;
 import uk.gov.digital.ho.hocs.casework.domain.exception.ApplicationExceptions;
