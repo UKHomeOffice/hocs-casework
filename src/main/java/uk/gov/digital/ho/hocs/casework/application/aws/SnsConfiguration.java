3a65972ba1bf8d22a105f460d5e572a77bfd318f
Stuart <stuart.whitlam@digital.homeoffice.gov.uk>
2019-03-19 14:00:24 +0000

    use queue config for region

--- src/main/java/uk/gov/digital/ho/hocs/casework/application/aws/SnsConfiguration.java
+++ src/main/java/uk/gov/digital/ho/hocs/casework/application/aws/SnsConfiguration.java
@@ -19,7 +19,7 @@ public class SnsConfiguration {
     @Bean("auditSnsClient")
     public AmazonSNS auditSnsClient(@Value("${audit.aws.sns.access.key}") String accessKey,
                                     @Value("${audit.aws.sns.secret.key}") String secretKey,
-                                    @Value("${aws.sns.region}") String region) {
+                                    @Value("${aws.sqs.region}") String region) {
 
         if (StringUtils.isEmpty(accessKey)) {
             throw new BeanCreationException("Failed to create SNS client bean. Need non-blank value for access key");
