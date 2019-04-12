28ab2fd6b96c610e50978e6c8dff6d3ced9f38bd
Nick Mcmahon <nick.mcmahon@digital.homeoffice.gov.uk>
2019-04-12 23:53:45 +0100

    Add all Exception logging

--- src/main/java/uk/gov/digital/ho/hocs/casework/domain/exception/ApplicationExceptions.java
+++ src/main/java/uk/gov/digital/ho/hocs/casework/domain/exception/ApplicationExceptions.java
@@ -8,6 +8,12 @@ public interface ApplicationExceptions {
         private final LogEvent event;
         private final LogEvent exception;
 
+        public EntityCreationException(String msg, LogEvent event, Object... args) {
+            super(String.format(msg, args));
+            this.event = event;
+            this.exception = null;
+        }
+
         public EntityCreationException(String msg, LogEvent event, LogEvent exception, Object... args) {
             super(String.format(msg, args));
             this.event = event;
