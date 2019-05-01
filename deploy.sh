c11ce987a6f481cc8eae28e4df890413049ee83a
Nick McMahon <nick.mcmahon@digital.homeoffice.gov.uk>
2019-05-01 21:58:57 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -15,15 +15,15 @@ else
     if [[ ${ENVIRONMENT} == "qa" ]] ; then
         echo "deploy ${VERSION} to QA namespace, using HOCS_CASEWORK_QA drone secret"
         export KUBE_TOKEN=${HOCS_CASEWORK_QA}
-        export REPLICAS="2"
+        export REPLICAS="1"
     elif [[ ${ENVIRONMENT} == "demo" ]] ; then
         echo "deploy ${VERSION} to DEMO namespace, using HOCS_CASEWORK_DEMO drone secret"
         export KUBE_TOKEN=${HOCS_CASEWORK_DEMO}
-        export REPLICAS="2"
+        export REPLICAS="1"
     elif [[ ${ENVIRONMENT} == "dev" ]] ; then
         echo "deploy ${VERSION} to DEV namespace, using HOCS_CASEWORK_DEV drone secret"
         export KUBE_TOKEN=${HOCS_CASEWORK_DEV}
-        export REPLICAS="2"        
+        export REPLICAS="1"        
     else
         echo "Unable to find environment: ${ENVIRONMENT}"
     fi
