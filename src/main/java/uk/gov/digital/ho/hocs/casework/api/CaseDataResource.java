0e6f83f2ddddcfcd44bbfb6ada57d54afb57d4c7
Nick Mcmahon <nick.mcmahon@digital.homeoffice.gov.uk>
2019-05-16 11:20:19 +0100

    Case notes for allocation/rejection

--- src/main/java/uk/gov/digital/ho/hocs/casework/api/CaseDataResource.java
+++ src/main/java/uk/gov/digital/ho/hocs/casework/api/CaseDataResource.java
@@ -62,9 +62,7 @@ class CaseDataResource {
     @GetMapping(value = "/case/{caseUUID}/timeline")
     ResponseEntity<Set<TimelineItemDto>> getCaseTimeline(@PathVariable UUID caseUUID) {
         Stream<TimelineItem> timeline = caseDataService.getCaseTimeline(caseUUID);
-        Set<TimelineItem> tset = timeline.collect(Collectors.toSet());
-        Set<TimelineItemDto> tdsto = tset.stream().map(TimelineItemDto::from).collect(Collectors.toSet());
-        return ResponseEntity.ok(tdsto);
+        return ResponseEntity.ok(timeline.map(TimelineItemDto::from).collect(Collectors.toSet()));
     }
 
     @Authorised(accessLevel = AccessLevel.SUMMARY)
