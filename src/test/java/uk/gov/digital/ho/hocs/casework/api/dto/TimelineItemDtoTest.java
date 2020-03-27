c45523dc12ea59095bfff679f05a2da8e65a5e71
Mark Wright <mark.wright@bjss.com>
2020-03-27 14:55:46 +0000

    HOCS-1640 : editing and deleting case notes API, edited & editor (5)

--- src/test/java/uk/gov/digital/ho/hocs/casework/api/dto/TimelineItemDtoTest.java
+++ src/test/java/uk/gov/digital/ho/hocs/casework/api/dto/TimelineItemDtoTest.java
@@ -19,6 +19,32 @@ public class TimelineItemDtoTest {
         String type = "CASE_NOTE";
         String body = "{some: data}";
         UUID timelineItemUUID = UUID.randomUUID();
+
+        TimelineItem timeline = new TimelineItem(caseUUID,stageUUID,eventTime,userName,type, body, timelineItemUUID, null, null);
+
+        TimelineItemDto timelineDto = TimelineItemDto.from(timeline);
+
+        assertThat(timelineDto.getCaseUUID()).isEqualTo(timeline.getCaseUUID());
+        assertThat(timelineDto.getStageUUID()).isEqualTo(timeline.getStageUUID());
+        assertThat(timelineDto.getEventTime().toLocalDateTime()).isEqualTo(timeline.getEventTime());
+        assertThat(timelineDto.getEventTime().getOffset()).isEqualTo(ZoneOffset.UTC);
+        assertThat(timelineDto.getUserName()).isEqualTo(timeline.getUserName());
+        assertThat(timelineDto.getBody()).isEqualTo(timeline.getMessage());
+        assertThat(timelineDto.getTimelineItemUUID()).isEqualTo(timeline.getTimelineItemUUID());
+        assertThat(timelineDto.getEditedTime()).isNull();
+        assertThat(timelineDto.getEditorName()).isNull();
+    }
+
+    @Test
+    public void getTimelineDtoTestWithEdit() {
+
+        UUID caseUUID = UUID.randomUUID();
+        UUID stageUUID = UUID.randomUUID();
+        LocalDateTime eventTime = LocalDateTime.of(2019,12,5,14,5,1,222);
+        String userName = "user";
+        String type = "CASE_NOTE";
+        String body = "{some: data}";
+        UUID timelineItemUUID = UUID.randomUUID();
         LocalDateTime editedTime = LocalDateTime.of(2020, 2, 1, 14, 15, 16, 777);
         String editorName = "Jackie Kennedy";
 
