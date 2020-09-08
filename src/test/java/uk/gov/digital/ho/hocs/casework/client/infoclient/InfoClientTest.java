0cd0b1ee0ba371cea0784c8ed0ef198d3a8962d1
hubert.raich <hubert.raich@bjss.com>
2020-09-08 12:59:48 +0100

    HOCS-1929 - Offline QA dropdown shows wrong team - added new endpoint to fetch users based on stage's team if stage is not allocated to a team the default stage team will be used -update UserDto to include 'id' field (mapping was missing when fetching from info)

--- src/test/java/uk/gov/digital/ho/hocs/casework/client/infoclient/InfoClientTest.java
+++ src/test/java/uk/gov/digital/ho/hocs/casework/client/infoclient/InfoClientTest.java
@@ -30,7 +30,7 @@ public class InfoClientTest {
     }
 
     @Test
-    public void getCorrespondentType(){
+    public void getCorrespondentType() {
 
         CorrespondentTypeDto correspondentTypeDto = new CorrespondentTypeDto();
         GetCorrespondentTypeResponse getCorrespondentType = new GetCorrespondentTypeResponse(new HashSet<>(Arrays.asList(correspondentTypeDto)));
@@ -47,7 +47,7 @@ public class InfoClientTest {
     }
 
     @Test
-    public void getTeamByStageAndText(){
+    public void getTeamByStageAndText() {
 
         TeamDto teamDto = new TeamDto();
         when(restHelper.get("infoService", "/team/stage/stageType/text/text", TeamDto.class)).thenReturn(teamDto);
@@ -61,9 +61,10 @@ public class InfoClientTest {
     }
 
     @Test
-    public void getDocumentTags(){
+    public void getDocumentTags() {
         List<String> tags = new ArrayList(Arrays.asList("tag"));
-        when(restHelper.get("infoService", "/caseType/TEST/documentTags", new ParameterizedTypeReference<List<String>>() {})).thenReturn(tags);
+        when(restHelper.get("infoService", "/caseType/TEST/documentTags", new ParameterizedTypeReference<List<String>>() {
+        })).thenReturn(tags);
 
         List<String> response = infoClient.getDocumentTags("TEST");
 
@@ -72,51 +73,54 @@ public class InfoClientTest {
     }
 
     @Test
-    public void getCaseDeadlineWarning(){
-        LocalDate expected = LocalDate.of(2020,5,13);
+    public void getCaseDeadlineWarning() {
+        LocalDate expected = LocalDate.of(2020, 5, 13);
         when(restHelper.get("infoService", "/caseType/TEST/deadlineWarning?received=2020-05-08&days=0", LocalDate.class)).thenReturn(expected);
 
-        LocalDate response = infoClient.getCaseDeadlineWarning("TEST",LocalDate.of(2020,5,8),0);
+        LocalDate response = infoClient.getCaseDeadlineWarning("TEST", LocalDate.of(2020, 5, 8), 0);
 
         assertThat(response).isNotNull();
         assertThat(response).isEqualTo(expected);
     }
 
     @Test
-    public void getStageDeadlineWarning(){
-        LocalDate expected = LocalDate.of(2020,5,13);
+    public void getStageDeadlineWarning() {
+        LocalDate expected = LocalDate.of(2020, 5, 13);
         when(restHelper.get("infoService", "/stageType/TEST/deadlineWarning?received=2020-05-08&caseDeadlineWarning=2020-05-09", LocalDate.class)).thenReturn(expected);
 
-        LocalDate response = infoClient.getStageDeadlineWarning("TEST",LocalDate.of(2020,5,8),LocalDate.of(2020,5,9));
+        LocalDate response = infoClient.getStageDeadlineWarning("TEST", LocalDate.of(2020, 5, 8), LocalDate.of(2020, 5, 9));
 
         assertThat(response).isNotNull();
         assertThat(response).isEqualTo(expected);
     }
 
     @Test
-    public void getEntityListTotalsReturnsEntityList(){
+    public void getEntityListTotalsReturnsEntityList() {
         EntityTotalDto entityTotalDto = new EntityTotalDto(new HashMap(), new HashMap());
         EntityDto<EntityTotalDto> entityDto = new EntityDto<EntityTotalDto>("simpleName", entityTotalDto);
         List<EntityDto<EntityTotalDto>> entityListTotals = new ArrayList();
         entityListTotals.add(entityDto);
-        when(restHelper.get("infoService", "/entity/list/listName", new ParameterizedTypeReference<List<EntityDto<EntityTotalDto>>>() {})).thenReturn(entityListTotals);
+        when(restHelper.get("infoService", "/entity/list/listName", new ParameterizedTypeReference<List<EntityDto<EntityTotalDto>>>() {
+        })).thenReturn(entityListTotals);
 
         List<EntityDto<EntityTotalDto>> response = infoClient.getEntityListTotals("listName");
 
         assertThat(response).isNotNull();
         assertThat(response.size()).isEqualTo(1);
-        verify(restHelper).get("infoService", "/entity/list/listName", new ParameterizedTypeReference<List<EntityDto<EntityTotalDto>>>() {});
+        verify(restHelper).get("infoService", "/entity/list/listName", new ParameterizedTypeReference<List<EntityDto<EntityTotalDto>>>() {
+        });
         verifyNoMoreInteractions(restHelper);
     }
 
 
     @Test
-    public void getPriorityPoliciesForCaseType(){
+    public void getPriorityPoliciesForCaseType() {
         String policyType = "POLICYB";
         String caseType = "CASE_TYPE_A";
         Map<String, String> config = Map.of("propertyB", "valueB");
         List<PriorityPolicyDto> priorityPolicyDtos = Collections.singletonList(new PriorityPolicyDto(policyType, caseType, config));
-        when(restHelper.get("infoService", "/priority/policy/" + caseType, new ParameterizedTypeReference<List<PriorityPolicyDto>>() {})).thenReturn(priorityPolicyDtos);
+        when(restHelper.get("infoService", "/priority/policy/" + caseType, new ParameterizedTypeReference<List<PriorityPolicyDto>>() {
+        })).thenReturn(priorityPolicyDtos);
 
 
         List<PriorityPolicyDto> results = infoClient.getPriorityPoliciesForCaseType(caseType);
@@ -127,28 +131,31 @@ public class InfoClientTest {
         assertThat(results.get(0).getCaseType()).isEqualTo(caseType);
         assertThat(results.get(0).getConfig()).isEqualTo(config);
 
-        verify(restHelper).get("infoService", "/priority/policy/" + caseType, new ParameterizedTypeReference<List<PriorityPolicyDto>>() {});
+        verify(restHelper).get("infoService", "/priority/policy/" + caseType, new ParameterizedTypeReference<List<PriorityPolicyDto>>() {
+        });
         verifyNoMoreInteractions(restHelper);
 
     }
 
     @Test
-    public void getWorkingDaysElapsedForCaseType(){
+    public void getWorkingDaysElapsedForCaseType() {
         String caseType = "CASE_TYPE_A";
         LocalDate fromDate = LocalDate.parse("2020-05-11");
-        when(restHelper.get("infoService", "/caseType/CASE_TYPE_A/workingDays/2020-05-11", new ParameterizedTypeReference<Integer>() {})).thenReturn(12);
+        when(restHelper.get("infoService", "/caseType/CASE_TYPE_A/workingDays/2020-05-11", new ParameterizedTypeReference<Integer>() {
+        })).thenReturn(12);
 
         Integer result = infoClient.getWorkingDaysElapsedForCaseType(caseType, fromDate);
         assertThat(result).isNotNull();
         assertThat(result).isEqualTo(12);
 
-        verify(restHelper).get("infoService", "/caseType/CASE_TYPE_A/workingDays/2020-05-11", new ParameterizedTypeReference<Integer>() {});
+        verify(restHelper).get("infoService", "/caseType/CASE_TYPE_A/workingDays/2020-05-11", new ParameterizedTypeReference<Integer>() {
+        });
         verifyNoMoreInteractions(restHelper);
     }
 
 
     @Test
-    public void getProfileByCaseType(){
+    public void getProfileByCaseType() {
 
         ProfileDto profileDto = new ProfileDto("prof1", true, List.of());
         when(restHelper.get("infoService", "/profile/forcasetype/caseTypeA", ProfileDto.class)).thenReturn(profileDto);
@@ -161,4 +168,40 @@ public class InfoClientTest {
         verifyNoMoreInteractions(restHelper);
     }
 
+    @Test
+    public void getUsersForTeam() {
+        List<UserDto> users = List.of(new UserDto(UUID.randomUUID().toString(), "username", "firstName", "lastName", "email@test.com"));
+
+        UUID teamUUID = UUID.randomUUID();
+        when(restHelper.get("infoService", "/teams/" + teamUUID.toString() + "/members", new ParameterizedTypeReference<List<UserDto>>() {
+        })).thenReturn(users);
+
+        List<UserDto> response = infoClient.getUsersForTeam(teamUUID);
+
+        assertThat(response).isNotNull();
+        assertThat(response).isEqualTo(users);
+
+        verify(restHelper).get("infoService", "/teams/" + teamUUID.toString() + "/members", new ParameterizedTypeReference<List<UserDto>>() {
+        });
+        verifyNoMoreInteractions(restHelper);
+    }
+
+    @Test
+    public void getUsersForTeamByStage() {
+        List<UserDto> users = List.of(new UserDto(UUID.randomUUID().toString(), "username", "firstName", "lastName", "email@test.com"));
+
+        UUID caseUUID = UUID.randomUUID();
+        UUID stageUUID = UUID.randomUUID();
+        when(restHelper.get("infoService", "/case/" + caseUUID + "/stage/" + stageUUID + "/team/members", new ParameterizedTypeReference<List<UserDto>>() {
+        })).thenReturn(users);
+
+        List<UserDto> response = infoClient.getUsersForTeamByStage(caseUUID, stageUUID);
+
+        assertThat(response).isNotNull();
+        assertThat(response).isEqualTo(users);
+
+        verify(restHelper).get("infoService", "/case/" + caseUUID + "/stage/" + stageUUID + "/team/members", new ParameterizedTypeReference<List<UserDto>>() {
+        });
+        verifyNoMoreInteractions(restHelper);
+    }
 }
