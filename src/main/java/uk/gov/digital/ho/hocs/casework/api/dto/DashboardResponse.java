25704d9b77cf93f61051c72bb9cebe58586ad88f
Liam MacPherson <11508628+LiamMacP@users.noreply.github.com>
2021-08-02 12:16:23 +0100

    HOCS-3013: add dashboard endpoint (#412)
    * HOCS-3013: add dashboard endpoint
    
    At present when the frontend makes a call to load the dashboard,
    the `/stage` endpoint is hit. This call returns more information
    then is actually required to show the dashboard. As such, this
    change adds a `/dashboard` endpoint that returns only the
    required information to show the dashboard. 
    
    As the information required is aggregated from a number of
    different tables, splitting these out to increase performance was
    ideal. This solution therefore queries each of the key pieces of
    information separately and zips them together to return
    information for each stage/team. As a result of these queries not
    returning a entity, we have to run native queries and reflect the
    values into a new model `Summary`.

---
+++ src/main/java/uk/gov/digital/ho/hocs/casework/api/dto/DashboardResponse.java
@@ -0,0 +1,31 @@
+package uk.gov.digital.ho.hocs.casework.api.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public final class DashboardResponse {
+
+    @JsonProperty("stages")
+    private final List<DashboardSummaryDto> dashboardSummaries;
+
+    public static DashboardResponse from(Map<UUID, Map<String, Integer>> dashboardStageSummary) {
+        return new DashboardResponse(dashboardStageSummary.entrySet().stream()
+                .map((stageSummary) -> new DashboardSummaryDto(stageSummary.getKey(), stageSummary.getValue()))
+                .collect(Collectors.toList()));
+    }
+
+    @AllArgsConstructor
+    @Getter
+    public static class DashboardSummaryDto {
+        private final UUID teamUuid;
+        private final Map<String, Integer> statistics;
+    }
+
+}
---
+++ src/main/java/uk/gov/digital/ho/hocs/casework/domain/repository/SummaryRepository.java
@@ -0,0 +1,92 @@
+package uk.gov.digital.ho.hocs.casework.domain.repository;
+
+import org.hibernate.query.NativeQuery;
+import org.hibernate.transform.Transformers;
+import org.hibernate.type.IntegerType;
+import org.hibernate.type.UUIDCharType;
+import org.springframework.stereotype.Component;
+import uk.gov.digital.ho.hocs.casework.domain.model.Summary;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+
+@Component
+public class SummaryRepository {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public List<Summary> findTeamsAndCaseCountByTeamUuidandCaseTypes(Set<UUID> teamUuidSet, Set<String> caseTypeSet) {
+        Query query = entityManager.createNativeQuery("SELECT st.team_uuid as teamUuid, count(*) FROM casework.stage st INNER JOIN casework.case_data cd ON st.case_uuid = cd.uuid WHERE NOT cd.deleted AND (st.team_uuid IN ?1 OR cd.type IN ?2) AND st.team_uuid IS NOT NULL GROUP BY st.team_uuid");
+        query.setParameter(1, teamUuidSet);
+        query.setParameter(2, caseTypeSet);
+
+        query.unwrap(NativeQuery.class)
+                .addScalar("teamUuid", UUIDCharType.INSTANCE)
+                .addScalar("count", IntegerType.INSTANCE)
+                .setResultTransformer(Transformers.aliasToBean(Summary.class));
+
+        return (List<Summary>) query
+                .getResultList();
+    };
+
+    public List<Summary> findUnallocatedCasesByTeam(Set<UUID> teamUuidSet) {
+        Query query = entityManager.createNativeQuery("SELECT st.team_uuid as teamUuid, COUNT(*) FROM casework.stage st WHERE st.team_uuid in ?1 AND st.user_uuid IS NULL GROUP BY st.team_uuid");
+        query.setParameter(1, teamUuidSet);
+
+        query.unwrap(NativeQuery.class)
+                .addScalar("teamUuid", UUIDCharType.INSTANCE)
+                .addScalar("count", IntegerType.INSTANCE)
+                .setResultTransformer(Transformers.aliasToBean(Summary.class));
+
+        return (List<Summary>) query
+                .getResultList();
+    }
+
+    public List<Summary> findOverdueCasesByTeam(Set<UUID> teamUuidSet) {
+        Query query = entityManager.createNativeQuery("SELECT st.team_uuid as teamUuid, COUNT(*) FROM casework.stage st WHERE st.team_uuid IN ?1 AND st.deadline < CURRENT_DATE GROUP BY st.team_uuid");
+        query.setParameter(1, teamUuidSet);
+
+        query.unwrap(NativeQuery.class)
+                .addScalar("teamUuid", UUIDCharType.INSTANCE)
+                .addScalar("count", IntegerType.INSTANCE)
+                .setResultTransformer(Transformers.aliasToBean(Summary.class));
+
+        return (List<Summary>) query
+                .getResultList();
+    }
+
+    public List<Summary> findOverdueUserCasesInTeams(Set<UUID> teamUuidSet, String userUuid) {
+        Query query = entityManager.createNativeQuery("SELECT st.team_uuid as teamUuid, COUNT(*) FROM casework.stage st WHERE st.team_uuid in ?1 AND st.user_uuid = ?2 AND st.deadline < CURRENT_DATE GROUP BY st.team_uuid");
+        query.setParameter(1, teamUuidSet);
+        query.setParameter(2, userUuid);
+
+
+        query.unwrap(NativeQuery.class)
+                .addScalar("teamUuid", UUIDCharType.INSTANCE)
+                .addScalar("count", IntegerType.INSTANCE)
+                .setResultTransformer(Transformers.aliasToBean(Summary.class));
+
+        return (List<Summary>) query
+                .getResultList();
+    }
+
+    public List<Summary> findUserCasesInTeams(Set<UUID> teamUuidSet, String userUuid) {
+        Query query = entityManager.createNativeQuery("SELECT CAST(st.team_uuid as varchar) as teamUuid, COUNT(*) FROM casework.stage st WHERE st.team_uuid in ?1 AND st.user_uuid = ?2 GROUP BY st.team_uuid");
+        query.setParameter(1, teamUuidSet);
+        query.setParameter(2, userUuid);
+
+        query.unwrap(NativeQuery.class)
+                .addScalar("teamUuid", UUIDCharType.INSTANCE)
+                .addScalar("count", IntegerType.INSTANCE)
+                .setResultTransformer(Transformers.aliasToBean(Summary.class));
+
+        return (List<Summary>) query
+                .getResultList();
+    }
+
+}
