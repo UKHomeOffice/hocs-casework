f951434efc41f328c1efdf5eca526567b71283c4
fewtrees-ho <robert.smallwood@digital.homeoffice.gov.uk>
2021-08-26 13:58:53 +0100

    HOCS-3556 - Add AbstractCaseCopyStrategy and test

--- src/main/java/uk/gov/digital/ho/hocs/casework/api/factory/strategies/CopyCompToComp2.java
+++ src/main/java/uk/gov/digital/ho/hocs/casework/api/factory/strategies/CopyCompToComp2.java
@@ -8,19 +8,53 @@ import uk.gov.digital.ho.hocs.casework.api.CorrespondentService;
 import uk.gov.digital.ho.hocs.casework.api.factory.CaseCopy;
 import uk.gov.digital.ho.hocs.casework.domain.model.CaseData;
 
-import java.util.Map;
-
 @Service
 @CaseCopy(fromCaseType = "COMP", toCaseType = "COMP2")
-public class CopyCompToComp2 implements CaseCopyStrategy {
+public class CopyCompToComp2 extends AbstractCaseCopyStrategy implements CaseCopyStrategy {
 
-    private ObjectMapper mapper;
+    private static final String[] DATA_CLOB_KEYS = {
+            "CatLost",
+            "CatRude",
+            "Channel",
+            "CatCCPhy",
+            "CatDamBF",
+            "CatDelay",
+            "CatFraud",
+            "CatTheft",
+            "CompType",
+            "Severity",
+            "CatRacism",
+            "CatUnfair",
+            "OwningCSU",
+            "CatAssault",
+            "CatCCAvail",
+            "3rdPartyRef",
+            "CaseSummary",
+            "CatAdminErr",
+            "CatCCHandle",
+            "CatPoorComm",
+            "PrevUkviRef",
+            "CatCustodyBF",
+            "CatWrongInfo",
+            "CatSexAssault",
+            "CatCCProvMinor",
+            "CatOtherUnprof",
+            "ComplainantDOB",
+            "ComplainantHORef",
+            "ComplainantGender",
+            "ComplainantPortRef",
+            "SeverityVulnerable",
+            "SeveritySafeGuarding",
+            "ComplainantCompanyName",
+            "ComplainantNationality"
+    };
+
     private CaseDataService caseDataService;
     private CorrespondentService correspondentService;
 
     @Autowired
     public CopyCompToComp2(ObjectMapper mapper, CaseDataService caseDataService, CorrespondentService correspondentService) {
-        this.mapper = mapper;
+        super(mapper);
         this.caseDataService = caseDataService;
         this.correspondentService = correspondentService;
     }
@@ -29,60 +63,12 @@ public class CopyCompToComp2 implements CaseCopyStrategy {
     public void copyCase(CaseData fromCase, CaseData toCase) {
 
         // copy clob details
-        copyClobData(fromCase, toCase);
+        copyClobData(fromCase, toCase, DATA_CLOB_KEYS);
         caseDataService.updateCaseData(toCase.getUuid(), null, toCase.getDataMap(mapper));
 
         // Correspondents include the primary_correspondent_uuid
         correspondentService.copyCorrespondents(fromCase.getUuid(), toCase.getUuid());
 
-    }
-
-    private void copyClobData(CaseData fromCase, CaseData toCase) {
-        String[] dataClobKeys = new String[]{
-                "CatLost",
-                "CatRude",
-                "Channel",
-                "CatCCPhy",
-                "CatDamBF",
-                "CatDelay",
-                "CatFraud",
-                "CatTheft",
-                "CompType",
-                "Severity",
-                "CatRacism",
-                "CatUnfair",
-                "OwningCSU",
-                "CatAssault",
-                "CatCCAvail",
-                "3rdPartyRef",
-                "CaseSummary",
-                "CatAdminErr",
-                "CatCCHandle",
-                "CatPoorComm",
-                "PrevUkviRef",
-                "CatCustodyBF",
-                "CatWrongInfo",
-                "CatSexAssault",
-                "CatCCProvMinor",
-                "CatOtherUnprof",
-                "ComplainantDOB",
-                "ComplainantHORef",
-                "ComplainantGender",
-                "ComplainantPortRef",
-                "SeverityVulnerable",
-                "SeveritySafeGuarding",
-                "ComplainantCompanyName",
-                "ComplainantNationality"
-        };
-
-        Map<String, String> fromCaseClobData = fromCase.getDataMap(mapper);
-        Map<String, String> toCaseClobData = toCase.getDataMap(mapper);
-
-        for (String dataClobKey : dataClobKeys) {
-            toCaseClobData.put(dataClobKey, fromCaseClobData.get(dataClobKey));
-        }
-
-        toCase.update(toCaseClobData, mapper);
     }
 
 }
