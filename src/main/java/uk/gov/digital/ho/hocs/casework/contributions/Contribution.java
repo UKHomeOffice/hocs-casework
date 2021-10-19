19f8f9dfc450212ec8bdb57ec1fc4e58489520b9
Liam MacPherson <liammacpherson1996@gmail.com>
2021-10-19 11:56:20 +0100

    HOCS-3848: add contribution POJO
    As we have multiple connotations for what a Contribution is, it is
    simpler to hand off the deserialization to Jackson and for us to tell it
     what a contribution can be. This POJO handles this.

---
+++ src/main/java/uk/gov/digital/ho/hocs/casework/contributions/Contribution.java
@@ -0,0 +1,65 @@
+package uk.gov.digital.ho.hocs.casework.contributions;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
+
+@Setter
+@AllArgsConstructor
+public class Contribution {
+
+    @JsonProperty("contributionDueDate")
+    @JsonAlias({"approvalRequestDueDate"})
+    @Getter
+    LocalDate dueDate;
+
+    @JsonProperty("contributionStatus")
+    @JsonAlias({"approvalRequestStatus"})
+    String status;
+
+    public ContributionStatus getStatus() {
+        return ContributionStatus.getContributionStatus(status);
+    }
+
+    public boolean isContribution() {
+        return !(dueDate == null && status == null);
+    }
+
+    enum ContributionStatus {
+        // The order of these values is important
+        // They are used for sorting by highest priority
+        NONE(List.of(""), ""), // lowest
+        CONTRIBUTION_RECEIVED(List.of("contributionReceived", "approvalRequestResponseReceived"), "Received"),
+        CONTRIBUTION_CANCELLED(List.of("contributionCancelled", "approvalRequestCancelled"), "Cancelled"),
+        CONTRIBUTION_DUE(List.of("contributionDue"), "Due"),
+        CONTRIBUTION_OVERDUE(List.of("contributionOverdue"), "Overdue"); // highest
+
+        private final List<String> status;
+        private final String displayedStatus;
+
+        ContributionStatus(List<String> status, String displayedStatus) {
+            this.status = status;
+            this.displayedStatus = displayedStatus;
+        }
+
+        public static ContributionStatus getContributionStatus(String status) {
+            if (status == null) {
+                return NONE;
+            }
+
+            return Arrays.stream(values()).filter(cs -> cs.status.contains(status))
+                    .findFirst()
+                    .orElse(NONE);
+        }
+
+        public String getDisplayedStatus() {
+            return displayedStatus;
+        }
+    }
+}
