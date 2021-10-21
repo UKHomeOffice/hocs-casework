36603922365f4a8de5a058ad22c8801779c86ae7
Laurie Bennett <84858704+LaurieBennett@users.noreply.github.com>
2021-10-21 14:21:02 +0100

    Hocs 3779 fix foi topic showing as UUID (#452)
    * HOCS-3779 added endpoint and methods to set topic UUID based on textUUID
    
    * HOCS-3779 fixed getting topic when TOpics field only has a textUUID
    
    * HOCS-3779 Moved updatePrimaryTopic with textUUID to a seperate method

--- src/main/java/uk/gov/digital/ho/hocs/casework/api/CaseDataService.java
+++ src/main/java/uk/gov/digital/ho/hocs/casework/api/CaseDataService.java
@@ -3,6 +3,7 @@ package uk.gov.digital.ho.hocs.casework.api;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -465,6 +466,20 @@ public class CaseDataService {
         log.debug("Updating Primary Topic for Case: {} Topic: {}", caseUUID, primaryTopicUUID);
         CaseData caseData = getCaseData(caseUUID);
         caseData.setPrimaryTopicUUID(primaryTopicUUID);
+        caseDataRepository.save(caseData);
+        auditClient.updateCaseAudit(caseData, stageUUID);
+        log.info("Updated Primary Topic for Case: {} Correspondent: {}", caseUUID, primaryTopicUUID, value(EVENT, PRIMARY_TOPIC_UPDATED));
+    }
+
+    void updatePrimaryTopicWithTextUUID(UUID caseUUID, UUID stageUUID, UUID primaryTopicUUID, UUID textUUID) {
+        log.debug("Updating Primary Topic for Case: {} Topic: {}", caseUUID, primaryTopicUUID);
+        CaseData caseData = getCaseData(caseUUID);
+        caseData.setPrimaryTopicUUID(primaryTopicUUID);
+        // If we have been passed a text UUID we should change the data to replace it with the topic UUID
+        if(caseData.getData().contains(textUUID.toString())){
+            caseData.setData(StringUtils.replace(caseData.getData(),
+                    textUUID.toString(), primaryTopicUUID.toString()));
+        }
         caseDataRepository.save(caseData);
         auditClient.updateCaseAudit(caseData, stageUUID);
         log.info("Updated Primary Topic for Case: {} Correspondent: {}", caseUUID, primaryTopicUUID, value(EVENT, PRIMARY_TOPIC_UPDATED));
