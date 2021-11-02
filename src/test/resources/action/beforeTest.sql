408455b0057023ea4a6ec14a2e5fb234b13b043c
Gary Newbolt <gary.newbolt@digital.homeoffice.gov.uk>
2021-11-02 08:54:34 +0000

    update sql for int tests

--- src/test/resources/action/beforeTest.sql
+++ src/test/resources/action/beforeTest.sql
@@ -4,7 +4,7 @@ INSERT INTO case_data (uuid, created, type, reference, data, primary_topic_uuid,
                        case_deadline, date_received, deleted)
 VALUES ('14915b78-6977-42db-b343-0915a7f412a1', '2018-12-14 12:00:00', 'TEST', 'TEST/9990190/18',
         '{"DateReceived": "2018-01-01"}', null, null, '2018-01-29', '2018-01-01', false),
-       ('bb915b78-6977-42db-b343-0915a7f412a1', '2018-12-14 12:00:00', 'TEST', 'TEST/9990190/18',
+       ('bb915b78-6977-42db-b343-0915a7f412a1', '2018-12-14 12:00:00', 'TEST', 'TEST/9990191/18',
         '{"DateReceived": "2018-01-01"}', null, null, '2018-01-29', '2018-01-01', false) ON CONFLICT DO NOTHING;
 
 INSERT INTO stage (uuid, created, type, deadline, case_uuid, team_uuid, user_uuid)
@@ -20,7 +20,7 @@ INSERT INTO correspondent (uuid, created, type, case_uuid, fullname, postcode, a
 VALUES ('2c9e1eb9-ee78-4f57-a626-b8b75cf3b937', '2018-12-14 12:00:00', 'Member', '14915b78-6977-42db-b343-0915a7f412a1',
         'Bob Someone', 'S1 1DJ', '1 SomeWhere Close', 'Sheffield', 'South Yorkshire', 'England', '01142595959',
         'a@a.com', '1', false),
-       ('2c9e1eb9-ee78-4f57-a626-b8b75cf3b937', '2018-12-14 12:00:00', 'Member', 'bb915b78-6977-42db-b343-0915a7f412a1',
+       ('bb9e1eb9-ee78-4f57-a626-b8b75cf3b937', '2018-12-14 12:00:00', 'Member', 'bb915b78-6977-42db-b343-0915a7f412a1',
         'Bob Someone', 'S1 1DJ', '1 SomeWhere Close', 'Sheffield', 'South Yorkshire', 'England', '01142595959',
         'a@a.com', '1', false)ON CONFLICT DO NOTHING;
 
