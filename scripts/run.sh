865c71d7c38d566498f22221ddf3054e2d66ccd7
Nick McMahon <nick.mcmahon@digital.homeoffice.gov.uk>
2021-11-03 16:05:35 +0000

    Update run.sh
    Remove unneeded {NAME}

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 
-NAME=${NAME:-casework}
-
 exec java ${JAVA_OPTS} -Dcom.sun.management.jmxremote.local.only=false -Djava.security.egd=file:/dev/./urandom org.springframework.boot.loader.JarLauncher
