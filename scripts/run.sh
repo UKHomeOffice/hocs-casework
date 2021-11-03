8bff0cfb460c4be579c13f1df39538ade7383bf7
Nick McMahon <nick.mcmahon@digital.homeoffice.gov.uk>
2021-11-03 13:46:18 +0000

    This commit moves us to a multi-stage docker build with a layered jar instead of a fat jar. The objective is to reduce the sprinboot startup time in k8s environments, a side effect is more efficent docker layer caching for developer flow.

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,5 +2,4 @@
 
 NAME=${NAME:-casework}
 
-JAR=$(find . -name ${NAME}*.jar|head -1)
-exec java ${JAVA_OPTS} -Dcom.sun.management.jmxremote.local.only=false -Djava.security.egd=file:/dev/./urandom -jar "${JAR}"
+exec java ${JAVA_OPTS} -Dcom.sun.management.jmxremote.local.only=false -Djava.security.egd=file:/dev/./urandom org.springframework.boot.loader.JarLauncher
