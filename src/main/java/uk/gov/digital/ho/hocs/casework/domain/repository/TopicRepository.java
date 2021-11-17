edd81e2d7d100949d766414c08a04b75af282d09
Nick McMahon <nick.mcmahon@digital.homeoffice.gov.uk>
2021-11-17 23:05:33 +0000

    HOCS-4178 Simplify topic 'findAllByCaseUUID' This commit replaces the use of active_case in 'findByUUID' to provide the original intent of the query as active_case has changed. This query returns a specific Topic for a given TopicUUID and CaseUUID that hasn't been soft deleted and isn't on a Case that has been soft deleted. This change reduces this query time from 1.3ms to 0.2ms

--- src/main/java/uk/gov/digital/ho/hocs/casework/domain/repository/TopicRepository.java
+++ src/main/java/uk/gov/digital/ho/hocs/casework/domain/repository/TopicRepository.java
@@ -14,7 +14,7 @@ public interface TopicRepository extends CrudRepository<Topic, Long> {
     @Query(value = "SELECT t.* FROM topic t JOIN case_data cd ON t.case_uuid = cd.uuid WHERE t.case_uuid = ?1 AND NOT t.deleted AND NOT cd.deleted", nativeQuery = true)
     Set<Topic> findAllByCaseUUID(UUID caseUUID);
 
-    @Query(value = "SELECT ato.* FROM active_topic ato WHERE ato.case_uuid = ?1 AND ato.uuid = ?2", nativeQuery = true)
+    @Query(value = "SELECT t.* FROM topic t JOIN case_data cd ON t.case_uuid = cd.uuid WHERE t.case_uuid = ?1 AND t.uuid = ?2 AND NOT t.deleted AND NOT cd.deleted", nativeQuery = true)
     Topic findByUUID(UUID caseUUID, UUID topicUUID);
 
     Set<Topic> findAll();
