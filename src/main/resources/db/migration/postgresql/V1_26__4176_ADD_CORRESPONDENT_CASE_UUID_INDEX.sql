1ff5d3e1b01089e574dac41365f17c473b766dca
Nick McMahon <nick.mcmahon@digital.homeoffice.gov.uk>
2021-11-18 00:03:38 +0000

    HOCS-4180 Simplify topic 'findAllByCaseUUID' This commit replaces the use of active_case in 'findAllByCaseUUID' to provide the original intent of the query as active_case has changed. This query returns all Correspondents by CaseUUID with an additional 'is_primary' flag that haven't been soft deleted and aren't on a Case that has been soft deleted. This change reduces the execution time from 84ms to 0.3ms (with index from previous checkin)

--- src/main/resources/db/migration/postgresql/V1_26__4176_ADD_CORRESPONDENT_CASE_UUID_INDEX.sql
+++ src/main/resources/db/migration/postgresql/V1_26__4176_ADD_CORRESPONDENT_CASE_UUID_INDEX.sql
@@ -1,3 +1,3 @@
 SET search_path TO casework;
 
-CREATE INDEX IF NOT EXISTS idx_correspondent_case_uuid ON correspondent (case_uuid)
+CREATE INDEX IF NOT EXISTS idx_correspondent_case_uuid ON correspondent (case_uuid);
