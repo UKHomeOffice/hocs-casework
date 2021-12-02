d869d6f65a7e0df3bec06bb9a8dc5a79ac61a9f3
Nick McMahon <nick.mcmahon@digital.homeoffice.gov.uk>
2021-12-02 15:24:36 +0000

    Update CaseDataRepository.java
    Remove timing comments.

--- src/main/java/uk/gov/digital/ho/hocs/casework/domain/repository/CaseDataRepository.java
+++ src/main/java/uk/gov/digital/ho/hocs/casework/domain/repository/CaseDataRepository.java
@@ -13,27 +13,21 @@ public interface CaseDataRepository extends CrudRepository<CaseData, Long> {
     @Query(value = "SELECT nextval('case_ref')", nativeQuery = true)
     Long getNextSeriesId();
 
-    // <0.2ms
     @Query(value = "SELECT ac.* FROM case_data ac where ac.uuid = ?1 AND ac.deleted = false", nativeQuery = true)
     CaseData findActiveByUuid(UUID uuid);
 
-    // <0.2ms
     @Query(value = "SELECT ac.* FROM case_data ac where ac.reference = ?1", nativeQuery = true)
     CaseData findByReference(String reference);
 
-    // <0.2ms
     @Query(value = "SELECT ac.reference FROM case_data ac where ac.uuid = ?1", nativeQuery = true)
     String getCaseRef(UUID uuid);
 
-    // <0.2ms
     @Query(value = "SELECT ac.type FROM case_data ac where ac.uuid = ?1", nativeQuery = true)
     String getCaseType(UUID uuid);
 
-    // <1ms
     @Query(value = "SELECT cd.* FROM view_case_data cd WHERE cd.uuid = ?1", nativeQuery = true)
     CaseData findAnyByUuid(UUID uuid);
 
-    // <0.2ms
     @Query(value = "SELECT ac.reference FROM case_data ac where ac.uuid = ?1", nativeQuery = true)
     String getCaseDataCaseRef(UUID uuid);
 }
