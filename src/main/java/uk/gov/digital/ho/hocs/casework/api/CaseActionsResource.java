4d88c82914c1ea9e38969b6c44c8a0dd12fc516f
Andrew Graham <andrew.graham@digital.homeoffice.gov.uk>
2021-12-14 13:23:01 +0000

    remove redundant import

--- src/main/java/uk/gov/digital/ho/hocs/casework/api/CaseActionsResource.java
+++ src/main/java/uk/gov/digital/ho/hocs/casework/api/CaseActionsResource.java
@@ -1,6 +1,5 @@
 package uk.gov.digital.ho.hocs.casework.api;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
