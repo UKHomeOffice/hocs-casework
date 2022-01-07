ef546927b0b8ccaec692360979d162d5548783da
Andrew Graham <andrew.graham@digital.homeoffice.gov.uk>
2022-01-07 21:04:34 +0000

    add chron job

--- kube/deploy.sh
+++ kube/deploy.sh
@@ -11,10 +11,15 @@ then
     export MIN_REPLICAS="2"
     export MAX_REPLICAS="3"
 
+    export BANK_HOLIDAY_REFRESH_HOUR="5"
+
+
     export UPTIME_PERIOD="Mon-Sun 05:00-23:00 Europe/London"
 else
     export MIN_REPLICAS="1"
     export MAX_REPLICAS="2"
+
+    export BANK_HOLIDAY_REFRESH_HOUR="8"
 
     export UPTIME_PERIOD="Mon-Fri 08:00-18:00 Europe/London"
 fi
