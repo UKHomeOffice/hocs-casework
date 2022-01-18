3fb3bff5112ca366a0a4b7b6096f545e882c00ac
Liam MacPherson <liammacpherson1996@gmail.com>
2022-01-18 13:06:02 +0000

    HOCS-4453: exclude deleted cases from dashboard
    When we collate all the statistics for the dashboard this currently
    includes any cases that are deleted. Any deleted cases should not be
    included within these as they are effectively 'removed' from the system.

--- src/main/java/uk/gov/digital/ho/hocs/casework/domain/repository/SummaryRepository.java
+++ src/main/java/uk/gov/digital/ho/hocs/casework/domain/repository/SummaryRepository.java
@@ -17,6 +17,9 @@ import java.util.UUID;
 @Component
 public class SummaryRepository {
 
+    private static final String TEAM_UUID_COLUMN_NAME = "teamUuid";
+    private static final String COUNT_COLUMN_NAME = "count";
+
     @PersistenceContext
     private EntityManager entityManager;
 
@@ -25,66 +28,61 @@ public class SummaryRepository {
         query.setParameter(1, teamUuidSet);
 
         query.unwrap(NativeQuery.class)
-                .addScalar("teamUuid", UUIDCharType.INSTANCE)
-                .addScalar("count", IntegerType.INSTANCE)
+                .addScalar(TEAM_UUID_COLUMN_NAME, UUIDCharType.INSTANCE)
+                .addScalar(COUNT_COLUMN_NAME, IntegerType.INSTANCE)
                 .setResultTransformer(Transformers.aliasToBean(Summary.class));
 
         return query.getResultList();
     }
 
     public List<Summary> findUnallocatedCasesByTeam(Set<UUID> teamUuidSet) {
-        Query query = entityManager.createNativeQuery("SELECT st.team_uuid as teamUuid, COUNT(*) FROM casework.stage st INNER JOIN casework.case_data cd ON st.case_uuid = cd.uuid WHERE st.team_uuid in ?1 AND st.user_uuid IS NULL AND NOT cd.data @> CAST('{\"Unworkable\":\"True\"}' AS JSONB) GROUP BY st.team_uuid");
+        Query query = entityManager.createNativeQuery("SELECT st.team_uuid as teamUuid, COUNT(*) FROM casework.stage st INNER JOIN casework.case_data cd ON st.case_uuid = cd.uuid WHERE st.team_uuid in ?1 AND st.user_uuid IS NULL AND NOT cd.deleted AND NOT cd.data @> CAST('{\"Unworkable\":\"True\"}' AS JSONB) GROUP BY st.team_uuid");
         query.setParameter(1, teamUuidSet);
 
         query.unwrap(NativeQuery.class)
-                .addScalar("teamUuid", UUIDCharType.INSTANCE)
-                .addScalar("count", IntegerType.INSTANCE)
+                .addScalar(TEAM_UUID_COLUMN_NAME, UUIDCharType.INSTANCE)
+                .addScalar(COUNT_COLUMN_NAME, IntegerType.INSTANCE)
                 .setResultTransformer(Transformers.aliasToBean(Summary.class));
 
-        return (List<Summary>) query
-                .getResultList();
+        return query.getResultList();
     }
 
     public List<Summary> findOverdueCasesByTeam(Set<UUID> teamUuidSet) {
-        Query query = entityManager.createNativeQuery("SELECT st.team_uuid as teamUuid, COUNT(*) FROM casework.stage st INNER JOIN casework.case_data cd ON st.case_uuid = cd.uuid WHERE st.team_uuid IN ?1 AND st.deadline < CURRENT_DATE AND NOT cd.data @> CAST('{\"Unworkable\":\"True\"}' AS JSONB) GROUP BY st.team_uuid");
+        Query query = entityManager.createNativeQuery("SELECT st.team_uuid as teamUuid, COUNT(*) FROM casework.stage st INNER JOIN casework.case_data cd ON st.case_uuid = cd.uuid WHERE st.team_uuid IN ?1 AND st.deadline < CURRENT_DATE AND NOT cd.deleted AND NOT cd.data @> CAST('{\"Unworkable\":\"True\"}' AS JSONB) GROUP BY st.team_uuid");
         query.setParameter(1, teamUuidSet);
 
         query.unwrap(NativeQuery.class)
-                .addScalar("teamUuid", UUIDCharType.INSTANCE)
-                .addScalar("count", IntegerType.INSTANCE)
+                .addScalar(TEAM_UUID_COLUMN_NAME, UUIDCharType.INSTANCE)
+                .addScalar(COUNT_COLUMN_NAME, IntegerType.INSTANCE)
                 .setResultTransformer(Transformers.aliasToBean(Summary.class));
 
-        return (List<Summary>) query
-                .getResultList();
+        return query.getResultList();
     }
 
     public List<Summary> findOverdueUserCasesInTeams(Set<UUID> teamUuidSet, String userUuid) {
-        Query query = entityManager.createNativeQuery("SELECT st.team_uuid as teamUuid, COUNT(*) FROM casework.stage st INNER JOIN casework.case_data cd ON st.case_uuid = cd.uuid WHERE st.team_uuid in ?1 AND st.user_uuid = ?2 AND st.deadline < CURRENT_DATE AND NOT cd.data @> CAST('{\"Unworkable\":\"True\"}' AS JSONB) GROUP BY st.team_uuid");
+        Query query = entityManager.createNativeQuery("SELECT st.team_uuid as teamUuid, COUNT(*) FROM casework.stage st INNER JOIN casework.case_data cd ON st.case_uuid = cd.uuid WHERE st.team_uuid in ?1 AND st.user_uuid = ?2 AND st.deadline < CURRENT_DATE AND NOT cd.deleted AND NOT cd.data @> CAST('{\"Unworkable\":\"True\"}' AS JSONB) GROUP BY st.team_uuid");
         query.setParameter(1, teamUuidSet);
         query.setParameter(2, userUuid);
 
-
         query.unwrap(NativeQuery.class)
-                .addScalar("teamUuid", UUIDCharType.INSTANCE)
-                .addScalar("count", IntegerType.INSTANCE)
+                .addScalar(TEAM_UUID_COLUMN_NAME, UUIDCharType.INSTANCE)
+                .addScalar(COUNT_COLUMN_NAME, IntegerType.INSTANCE)
                 .setResultTransformer(Transformers.aliasToBean(Summary.class));
 
-        return (List<Summary>) query
-                .getResultList();
+        return query.getResultList();
     }
 
     public List<Summary> findUserCasesInTeams(Set<UUID> teamUuidSet, String userUuid) {
-        Query query = entityManager.createNativeQuery("SELECT CAST(st.team_uuid as varchar) as teamUuid, COUNT(*) FROM casework.stage st INNER JOIN casework.case_data cd ON st.case_uuid = cd.uuid WHERE st.team_uuid in ?1 AND st.user_uuid = ?2 AND NOT cd.data @> CAST('{\"Unworkable\":\"True\"}' AS JSONB) GROUP BY st.team_uuid");
+        Query query = entityManager.createNativeQuery("SELECT CAST(st.team_uuid as varchar) as teamUuid, COUNT(*) FROM casework.stage st INNER JOIN casework.case_data cd ON st.case_uuid = cd.uuid WHERE st.team_uuid in ?1 AND st.user_uuid = ?2 AND NOT cd.deleted AND NOT cd.data @> CAST('{\"Unworkable\":\"True\"}' AS JSONB) GROUP BY st.team_uuid");
         query.setParameter(1, teamUuidSet);
         query.setParameter(2, userUuid);
 
         query.unwrap(NativeQuery.class)
-                .addScalar("teamUuid", UUIDCharType.INSTANCE)
-                .addScalar("count", IntegerType.INSTANCE)
+                .addScalar(TEAM_UUID_COLUMN_NAME, UUIDCharType.INSTANCE)
+                .addScalar(COUNT_COLUMN_NAME, IntegerType.INSTANCE)
                 .setResultTransformer(Transformers.aliasToBean(Summary.class));
 
-        return (List<Summary>) query
-                .getResultList();
+        return query.getResultList();
     }
 
 }
