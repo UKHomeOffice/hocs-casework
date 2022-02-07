7313217dfff9f4985a40b09a7dc0e967d3e46347
Robert Deniszczyc <robert.deniszczyc@digital.homeoffice.gov.uk>
2022-02-07 21:52:04 +0000

    HOCS-4185 BF Stage 2

---
+++ src/main/java/uk/gov/digital/ho/hocs/casework/api/factory/strategies/CopyBfToBf2.java
@@ -0,0 +1,42 @@
+package uk.gov.digital.ho.hocs.casework.api.factory.strategies;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import uk.gov.digital.ho.hocs.casework.api.CaseDataService;
+import uk.gov.digital.ho.hocs.casework.api.CorrespondentService;
+import uk.gov.digital.ho.hocs.casework.api.factory.CaseCopy;
+import uk.gov.digital.ho.hocs.casework.domain.model.CaseData;
+
+@Service
+@CaseCopy(fromCaseType = "BF", toCaseType = "BF2")
+public class CopyBfToBf2 extends AbstractCaseCopyStrategy implements CaseCopyStrategy {
+
+    private static final String[] DATA_CLOB_KEYS = {
+            "BusArea",
+            "ComplainantCompanyName",
+            "ComplainantNationality"
+    };
+
+    private final CaseDataService caseDataService;
+    private final CorrespondentService correspondentService;
+
+    @Autowired
+    public CopyBfToBf2(CaseDataService caseDataService, CorrespondentService correspondentService) {
+        super();
+        this.caseDataService = caseDataService;
+        this.correspondentService = correspondentService;
+    }
+
+    @Override
+    public void copyCase(CaseData fromCase, CaseData toCase) {
+
+        // copy clob details
+        copyClobData(fromCase, toCase, DATA_CLOB_KEYS);
+        caseDataService.updateCaseData(toCase.getUuid(), null, toCase.getDataMap());
+
+        // Correspondents include the primary_correspondent_uuid
+        correspondentService.copyCorrespondents(fromCase.getUuid(), toCase.getUuid());
+
+    }
+
+}
