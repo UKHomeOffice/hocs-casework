0f3229f01a4c3f13e044f9e0e84b3df70be55392
Andrew Barker <andrew.barker@digital.homeoffice.gov.uk>
2022-04-25 13:08:17 +0100

    HOCS-4596 review comments update

--- src/main/java/uk/gov/digital/ho/hocs/casework/api/dto/CaseActionDataResponseDto.java
+++ src/main/java/uk/gov/digital/ho/hocs/casework/api/dto/CaseActionDataResponseDto.java
@@ -12,7 +12,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Getter
 public class CaseActionDataResponseDto {
 
