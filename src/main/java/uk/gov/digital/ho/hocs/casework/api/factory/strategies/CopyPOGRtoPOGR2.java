96a3c9cae763d59303e008be08f82876f10a1ed9
Joseph Rogerson <92527240+JosephRogerson@users.noreply.github.com>
2022-08-02 15:34:55 +0100

    HOCS-4791: Add ability to create POGR2 cases (#704)
    * HOCS-4791: Add ability to create POGR2 cases
    
    * HOCS-4791: Remove somu copy
    
    * HOCS-4791: Remove somu copy

---
+++ src/main/java/uk/gov/digital/ho/hocs/casework/api/factory/strategies/CopyPOGRtoPOGR2.java
@@ -0,0 +1,50 @@
+package uk.gov.digital.ho.hocs.casework.api.factory.strategies;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import uk.gov.digital.ho.hocs.casework.api.CaseDataService;
+import uk.gov.digital.ho.hocs.casework.api.CorrespondentService;
+import uk.gov.digital.ho.hocs.casework.api.factory.CaseCopy;
+import uk.gov.digital.ho.hocs.casework.domain.model.CaseData;
+
+@Service
+@CaseCopy(fromCaseType = "POGR", toCaseType = "POGR2")
+public class CopyPOGRtoPOGR2 extends AbstractCaseCopyStrategy implements CaseCopyStrategy {
+
+    private static final String[] DATA_CLOB_KEYS = {
+            "BusArea",
+            "ComplainantDOB",
+            "ComplainantGender",
+            "ComplainantNationality",
+            "ComplainantCompanyName",
+            "ComplainantCaseAccountNumber",
+            "ComplainantCategory",
+            "ComplainantNro",
+            "ComplainantLocation",
+            "ComplaintDescription",
+            "ComplaintChannel",
+            "ComplaintPriority",
+            "ComplaintThirdPartyReference",
+            "LoaRequired",
+            "LoaReceived"
+    };
+
+    private final CaseDataService caseDataService;
+    private final CorrespondentService correspondentService;
+
+    @Autowired
+    public CopyPOGRtoPOGR2(CaseDataService caseDataService, CorrespondentService correspondentService) {
+        super();
+        this.caseDataService = caseDataService;
+        this.correspondentService = correspondentService;
+    }
+
+    @Override
+    public void copyCase(CaseData fromCase, CaseData toCase) {
+        copyClobData(fromCase, toCase, DATA_CLOB_KEYS);
+        caseDataService.updateCaseData(toCase.getUuid(), null, toCase.getDataMap());
+
+        correspondentService.copyCorrespondents(fromCase.getUuid(), toCase.getUuid());
+    }
+
+}
---
+++ src/test/java/uk/gov/digital/ho/hocs/casework/api/factory/strategies/CopyPOGRToPOGR2Test.java
@@ -0,0 +1,132 @@
+package uk.gov.digital.ho.hocs.casework.api.factory.strategies;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import uk.gov.digital.ho.hocs.casework.api.CaseDataService;
+import uk.gov.digital.ho.hocs.casework.api.CorrespondentService;
+import uk.gov.digital.ho.hocs.casework.domain.model.Address;
+import uk.gov.digital.ho.hocs.casework.domain.model.CaseData;
+import uk.gov.digital.ho.hocs.casework.domain.model.Correspondent;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyMap;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+public class CopyPOGRToPOGR2Test {
+
+    private static final Map<String, String> FROM_CLOB;
+
+    static {
+        FROM_CLOB = Map.of(
+                "BusArea", "BusAreaValue",
+                "ComplainantLocation","ComplainantLocationValue",
+                "ComplaintChannel","ComplaintChannelValue",
+                "LoaRequired","LoaRequiredValue");
+    }
+
+    private static final UUID TO_CASE_UUID = UUID.randomUUID();
+    private static final UUID FROM_CASE_UUID = UUID.randomUUID();
+    private static final String CORRESPONDENT_TYPE = "correspondent_type";
+    private static final String FULLNAME = "fullname";
+    private static final String ORGANISATION = "organisation";
+    private static final String ADDRESS_1 = "address1";
+    private static final String ADDRESS_2 = "address2";
+    private static final String ADDRESS_3 = "address3";
+    private static final String POSTCODE = "postcode";
+    private static final String COUNTRY = "country";
+    private static final String TELEPHONE = "telephone";
+    private static final String EMAIL = "email";
+    private static final String REFERENCE = "reference";
+    private static final String EXTERNAL_KEY = "externalKey";
+    private static final Correspondent PRIMARY_CORRESPONDENT = new Correspondent(FROM_CASE_UUID,
+            CORRESPONDENT_TYPE,
+            FULLNAME,
+            ORGANISATION,
+            Address.builder()
+                    .address1(ADDRESS_1)
+                    .address2(ADDRESS_2)
+                    .address3(ADDRESS_3)
+                    .postcode(POSTCODE)
+                    .country(COUNTRY)
+                    .build(),
+            TELEPHONE,
+            EMAIL,
+            REFERENCE,
+            EXTERNAL_KEY);
+
+    private static final CaseData FROM_CASE = new CaseData(1L,
+            FROM_CASE_UUID,
+            null,
+            null,
+            null,
+            false,
+            FROM_CLOB,
+            null,
+            null,
+            PRIMARY_CORRESPONDENT.getUuid(),
+            PRIMARY_CORRESPONDENT,
+            null,
+            null,
+            null,
+            false,
+            null,
+            null);
+
+    @Mock
+    private CaseDataService caseDataService;
+
+    @Mock
+    private CorrespondentService correspondentService;
+
+    private CaseData toCase;
+
+    @Before
+    public void setUp() {
+        toCase = new CaseData(2L,
+                TO_CASE_UUID,
+                null,
+                null,
+                null,
+                false,
+                new HashMap<>(Map.of()),
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                false,
+                null,
+                null);
+    }
+
+    @Test
+    public void shouldCopyCaseDetails() {
+
+        // given
+        var pogrToPogr2 = new CopyPOGRtoPOGR2(caseDataService, correspondentService);
+
+        // when
+        pogrToPogr2.copyCase(FROM_CASE, toCase);
+
+        // then
+        verify(caseDataService, times(1)).updateCaseData(eq(toCase.getUuid()), any(), anyMap());
+        verify(correspondentService, times(1)).copyCorrespondents(FROM_CASE.getUuid(), toCase.getUuid());
+
+        assertThat(toCase.getDataMap()).isNotNull();
+        assertThat(toCase.getDataMap()).isEqualTo(FROM_CASE.getDataMap());
+
+    }
+}
