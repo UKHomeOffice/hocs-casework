75be91d4b0eb79e1a35a4202f97ecb767be9cb09
Liam MacPherson <liam.macpherson@digital.homeoffice.gov.uk>
2022-10-04 14:41:42 +0100

    HOCS-5679: utilise document tag configs
    Add a new document tag repository that replaces the existing
    hocs-info-service call to retrieve the document tags for a specific case
    type.

---
+++ src/main/java/uk/gov/digital/ho/hocs/casework/domain/repository/CaseTypeDocumentTagRepository.java
@@ -0,0 +1,44 @@
+package uk.gov.digital.ho.hocs.casework.domain.repository;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import uk.gov.digital.ho.hocs.casework.domain.model.CaseTypeDocumentTags;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import static net.logstash.logback.argument.StructuredArguments.value;
+import static uk.gov.digital.ho.hocs.casework.application.LogEvent.CASE_DATA_DETAILS_NOT_FOUND;
+import static uk.gov.digital.ho.hocs.casework.application.LogEvent.EVENT;
+
+@Service
+@Slf4j
+public class CaseTypeDocumentTagRepository extends JsonConfigFolderReader {
+
+    public final Map<String, List<String>> documentTags;
+
+    public CaseTypeDocumentTagRepository(ObjectMapper objectMapper) {
+        super(objectMapper);
+
+        documentTags = readValueFromFolder(new TypeReference<CaseTypeDocumentTags>() {});
+    }
+
+    public List<String> getTagsByType(String type) {
+        var tags = documentTags.get(type);
+
+        if (tags == null) {
+            log.warn("No mapping found for type: {}", type, value(EVENT, CASE_DATA_DETAILS_NOT_FOUND));
+            return Collections.emptyList();
+        }
+        return tags;
+    }
+
+    @Override
+    String getFolderName() {
+        return "document-tags";
+    }
+
+}
