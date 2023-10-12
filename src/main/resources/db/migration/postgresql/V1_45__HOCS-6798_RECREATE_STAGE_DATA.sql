2c32c0cb5855c0429a5089981fba4a13037968bf
vinothon84 <vinothon84@gmail.com>
2023-10-12 18:03:40 +0100

    HOCS-6798: Add active_case view to get recreated again

--- src/main/resources/db/migration/postgresql/V1_45__HOCS-6798_RECREATE_STAGE_DATA.sql
+++ src/main/resources/db/migration/postgresql/V1_45__HOCS-6798_RECREATE_STAGE_DATA.sql
@@ -2,9 +2,7 @@
 
 SET search_path TO casework;
 
-DROP VIEW IF EXISTS stage_data;
-
-create view stage_data
+create or replace view stage_data
             (case_reference, case_type, data, case_created, id, uuid, created, type, deadline, transition_note_uuid,
              case_uuid, team_uuid, user_uuid, deadline_warning, correspondents, case_assigned_topic, somu,
              secondary_case_uuid, secondary_case_reference, date_completed, secondary_stage_uuid)
@@ -61,3 +59,53 @@ FROM casework.stage s
                              LIMIT 1) sc ON true
          LEFT JOIN casework.topic t ON c.primary_topic_uuid = t.uuid
 WHERE NOT c.deleted;
+
+create or replace view active_case
+            (id, uuid, created, type, reference, data, primary_topic_uuid, primary_correspondent_uuid, case_deadline,
+             date_received, deleted, date_completed, case_deadline_warning, secondary_case_uuid, secondary_case_reference,
+             secondary_stage_uuid, primary_case_uuid, primary_case_reference, primary_stage_uuid, migrated_reference)
+AS
+SELECT case_data.id,
+       case_data.uuid,
+       case_data.created,
+       case_data.type,
+       case_data.reference,
+       case_data.data,
+       case_data.primary_topic_uuid,
+       case_data.primary_correspondent_uuid,
+       case_data.case_deadline,
+       case_data.date_received,
+       case_data.deleted,
+       case_data.date_completed,
+       case_data.case_deadline_warning,
+       secondary_case.uuid       AS secondary_case_uuid,
+       secondary_case.reference  AS secondary_case_reference,
+       secondary_case.stage_uuid AS secondary_stage_uuid,
+       primary_case.uuid         AS primary_case_uuid,
+       primary_case.reference    AS primary_case_reference,
+       primary_case.stage_uuid   AS primary_stage_uuid,
+       case_data.migrated_reference
+FROM casework.case_data
+         LEFT JOIN LATERAL ( SELECT c.reference,
+                                    c.uuid,
+                                    s.uuid AS stage_uuid,
+                                    s.team_uuid,
+                                    c.created
+                             FROM casework.case_link cl
+                                      LEFT JOIN casework.case_data c ON cl.secondary_case_uuid = c.uuid
+                                      LEFT JOIN casework.stage s ON s.case_uuid = c.uuid
+                             WHERE cl.primary_case_uuid = case_data.uuid
+                             ORDER BY s.team_uuid DESC NULLS LAST, s.created DESC
+                             LIMIT 1) secondary_case ON TRUE
+         LEFT JOIN LATERAL ( SELECT c.reference,
+                                    c.uuid,
+                                    s.uuid AS stage_uuid,
+                                    s.team_uuid,
+                                    c.created
+                             FROM casework.case_link cl
+                                      LEFT JOIN casework.case_data c ON cl.primary_case_uuid = c.uuid
+                                      LEFT JOIN casework.stage s ON s.case_uuid = c.uuid
+                             WHERE cl.secondary_case_uuid = case_data.uuid
+                             ORDER BY s.team_uuid DESC NULLS LAST, s.created DESC
+                             LIMIT 1) primary_case ON TRUE
+WHERE NOT case_data.deleted;
